33645eafc6341a53cc0cff7b92f161feee0c6db3
estherkang14 <estherkang14@gmail.com>
2020-06-04 15:49:41 -0400

    passed initialize method tests

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,38 @@
 # your code goes here
+require 'pry'
+
 class Person
+    attr_reader :name, :happiness, :hygiene 
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+       if happiness > 10 
+            @happiness = 10
+       elsif happiness < 0 
+            @happiness = 0 
+       else
+            @happiness = happiness
+       end 
+    end 
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10 
+        elsif hygiene < 0 
+            @hygiene = 0 
+        else 
+            @hygiene = hygiene 
+        end 
+    end
 
 
 
-    
+
 end
