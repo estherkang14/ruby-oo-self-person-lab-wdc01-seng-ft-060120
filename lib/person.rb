588856410f3f2b26bcf1ac556b3fe880e5c88c8e
Issa sangare <issabmsangare@hotmail.com>
2020-06-04 17:01:20 -0400

    solved all methods except the last one

--- lib/person.rb
+++ lib/person.rb
@@ -32,7 +32,45 @@ class Person
         end 
     end
 
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
 
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
 
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
 
+    def take_bath
+        #binding.pry
+        self.hygiene = @hygiene + 4
+        #self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness = @happiness + 2
+        self.hygiene = @hygiene - 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness = @happiness + 3
+        friend.happiness = friend.happiness + 3
+       
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+        #binding.pry
+    end
 end
